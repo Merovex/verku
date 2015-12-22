32df87be07c154b70b4743d96573f696b92fc61b
Merovex <dausha@gmail.com>
2015-12-22 18:19:04 -0500

    Error checking, lite

--- lib/bookmaker/version.rb
+++ lib/bookmaker/version.rb
@@ -2,8 +2,8 @@ module Bookmaker
   module Version
     MAJOR = 0
     MINOR = 7
-    PATCH = 0
-    BUILD = 'pre9'
+    PATCH = 1
+    BUILD = 'pre1'
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
