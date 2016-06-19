7e0a822c7b1ac612bb23980dcb5d5a70be10fa7c
Merovex <dausha@gmail.com>
2016-06-19 00:10:43 -0400

    Hasty Commit: 2016-06-19 00:10:42

--- lib/verku/version.rb
+++ lib/verku/version.rb
@@ -2,8 +2,8 @@ module Verku
   module Version
     MAJOR = 0
     MINOR = 8
-    PATCH = 0
-    BUILD = 'p'
+    PATCH = 1
+    BUILD = 0
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
